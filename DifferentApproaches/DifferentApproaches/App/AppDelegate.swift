bed351ff6342136efb808e8b347e0c1dc6896707
Yan Moroz <mail@yanmoroz.ru>
2024-02-22 03:59:14 +0300

    Clean code

--- DifferentApproaches/DifferentApproaches/App/AppDelegate.swift
+++ DifferentApproaches/DifferentApproaches/App/AppDelegate.swift
@@ -6,55 +6,12 @@
 //
 
 import UIKit
-import OSLog
-
-extension Error {
-    func log() {
-        Logger().error("\(self.localizedDescription)")
-    }
-}
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        Task {
-            do {
-                let games = try await MMOBombApiServiceImpl().getAllGames()
-                print(games.count)
-            } catch {
-                error.log()
-            }
-        }
-        
-        Task {
-            do {
-                let game = try await MMOBombApiServiceImpl().getGameById(456)
-                print(game.title)
-            } catch {
-                error.log()
-            }
-        }
-        
-        Task {
-            do {
-                let cards = try await MTGApiServiceImpl().getAllCards()
-                print(cards.count)
-            } catch {
-                error.log()
-            }
-        }
-        
-        Task {
-            do {
-                let card = try await MTGApiServiceImpl().getCardById(129711)
-                print(card.name)
-            } catch {
-                error.log()
-            }
-        }
         
         return true
     }
---
+++ DifferentApproaches/DifferentApproaches/Extensions/Error+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Error+Extension.swift
+//  DifferentApproaches
+//
+//  Created by Yan Moroz on 22.02.2024.
+//
+
+import OSLog
+
+extension Error {
+    func log() {
+        Logger().error("\(self.localizedDescription)")
+    }
+}
